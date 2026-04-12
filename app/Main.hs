9a80fd7497f4cffff5bd518c0e838df37c0dc794
MaciejBorowiecki <maciek.b.cki@gmail.com>
2026-04-12 14:23:18 +0200

    add show instances and name conflict resolving

--- app/Main.hs
+++ app/Main.hs
@@ -5,20 +5,37 @@ import Language.Haskell.Syntax
 import System.Environment
 import qualified Data.Map as Map
 
-data Def = Def Name [Pat] Expr deriving Show
-data Expr = Var Name | Expr :$ Expr deriving Show
+data Def = Def Name [Pat] Expr 
+data Expr = Var Name | Expr :$ Expr
 type Pat = Name
 type Name = String
-newtype Prog = Prog {progDefs :: [Def]} deriving Show
+newtype Prog = Prog {progDefs :: [Def]}
 type DefMap = Map.Map Name Def
 
+instance Show Expr where
+  showsPrec _ (Var n) = showString n
+  showsPrec p (e1 :$ e2) = showParen (p > 10) (showsPrec 10 e1 . showString " " . showsPrec 11 e2)
+
+instance Show Def where
+  show (Def name params expr) = 
+    unwords (name : params) ++ " = " ++ show expr
+
+instance Show Prog where
+  show (Prog defs) = unlines [show d | d <- defs]
+  
 main :: IO ()
 main = do
   args <- getArgs
   case args of 
     ["--help"] -> usage
     [] -> getContents >>= putStrLn
-    [f] -> readFile f >>= putStrLn
+    [f] -> do
+      code <- readFile f
+      let prog = fromHsString code
+      putStrLn "--- Def list (prog) ---"
+      putStr (show prog)
+      putStrLn "\n--- DefMap  ---"
+      putStrLn (show (buildDefMap prog))
     _ -> usage
 
 
@@ -62,17 +79,50 @@ fromHsExpr (HsApp e1 e2) = fromHsExpr e1 :$ fromHsExpr e2
 fromHsExpr (HsParen e) = fromHsExpr e
 fromHsExpr _ = error "fromHsExpr"
 
--- reduction utils
-
-
 insertDefMapHelper :: Def -> DefMap -> DefMap
 insertDefMapHelper (Def name args expr) dmap = Map.insert name (Def name args expr) dmap
 
 buildDefMap :: Prog -> DefMap
 buildDefMap (Prog defs) = foldr insertDefMapHelper Map.empty defs
 
--- insertDefMapHelper :: Def -> DefMap -> DefMap
--- insertDefMapHelper (Def name args expr) acc = Map.insert name (Def name args expr) acc
---
--- buildDefMap :: Prog -> DefMap
--- buildDefMap (Prog defs) = foldr insertDefMapHelper Map.empty defs
+-- name conflicts
+
+-- list of all names in expression
+allVars :: Expr -> [Name]
+allVars (Var n) = [n]
+allVars (e1 :$ e2) = allVars e1 ++ allVars e2
+
+-- True if name is in the list, False otherwise
+checkName :: Name -> [Name] -> Bool
+checkName name (x:xs) = if name == x 
+  then True
+  else checkName name xs
+checkName name [] = False
+
+-- get the new name for `p` if `p` taken
+freeName :: Name -> [Name] -> Name
+freeName name takenNames = if checkName name takenNames 
+  then freeName (name ++ "'") takenNames
+  else name
+
+-- change all occurences of old to new 
+subst :: (Name, Expr) -> Expr -> Expr
+subst (old, new) (Var n)
+  | n == old = new
+  | otherwise = Var n
+subst (old, new) (e1 :$ e2) = subst (old, new) e1 :$ subst (old, new) e2
+
+-- change one param from params to not taken name
+renameStep :: (Expr, [Name], [Name]) -> Name -> (Expr, [Name], [Name])
+renameStep (body, forbidden, newParams) oldP =
+  let newP = freeName oldP forbidden
+      updatedForbidden = newP : forbidden
+      updatedBody = subst (oldP, Var newP) body
+  in (updatedBody, updatedForbidden, newP : newParams)
+
+-- change all params to free names, reverse for keeping params order
+renameDef :: Def -> [Name] -> Def
+renameDef (Def dName params body) forbidden =
+  let newForbidden = forbidden ++ params 
+      (finalBody, _, finalParams) = foldl renameStep (body, newForbidden, []) params
+  in Def dName (reverse finalParams) finalBody
